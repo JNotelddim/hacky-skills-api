0568d527a81e7981da62b691073f0af1b87bd536
JNotelddim <JNotelddim@users.noreply.github.com>
2023-03-06 12:39:21 +0000

    Add basic json body parsing for post reqs, add one endpoint

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.post("/createEntry", (req, res) => {
+  const { title, description, tags, date } = req.body;
+  console.log({ title, description, tags, date });
+
+  res.send("new Entry , ...");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
